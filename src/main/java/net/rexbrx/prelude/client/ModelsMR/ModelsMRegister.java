5e0ecc4720995ed75f6e5674b664a4ca700f8acf
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2025-12-02 21:36:10 -0300

    pain part 4

---
+++ src/main/java/net/rexbrx/prelude/client/ModelsMR/ModelsMRegister.java
@@ -0,0 +1,15 @@
+package net.rexbrx.prelude.client.ModelsMR;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.rexbrx.prelude.client.ModelsMR.vat.VatModel;
+
+@EventBusSubscriber(value = {Dist.CLIENT})
+public class ModelsMRegister {
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(VatModel.LAYER_LOCATION, VatModel::createBodyLayer);
+    }
+}
