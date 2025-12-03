a274ccdfd995549b79caa8bbaf97bebefeade48b
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2025-12-03 17:47:06 -0300

    pain part 4

---
+++ src/main/java/net/rexbrx/prelude/server/compat/JEIPreludePlugin.java
@@ -0,0 +1,69 @@
+package net.rexbrx.prelude.server.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.rexbrx.prelude.client.menu.AnalyzerScreen;
+import net.rexbrx.prelude.client.menu.CrusherScreen;
+import net.rexbrx.prelude.client.menu.IncubatorScreen;
+import net.rexbrx.prelude.client.menu.VatScreen;
+import net.rexbrx.prelude.prelude;
+import net.rexbrx.prelude.server.recipes.ModRecipes;
+import net.rexbrx.prelude.server.recipes.common.AnalyzerRecipe;
+import net.rexbrx.prelude.server.recipes.common.CrusherRecipe;
+import net.rexbrx.prelude.server.recipes.common.VatRecipe;
+import net.rexbrx.prelude.server.recipes.common.incubatorRecipe;
+
+import java.util.List;
+
+@JeiPlugin
+public class JEIPreludePlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(prelude.MODID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new AnalyzerCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new VatCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new CrusherCategory(registration.getJeiHelpers().getGuiHelper()));
+        registration.addRecipeCategories(new IncubatorCategory(registration.getJeiHelpers().getGuiHelper()));
+
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
+
+        List<AnalyzerRecipe> AnalyzerRecipes = recipeManager.getAllRecipesFor(ModRecipes.ANALYZER_TYPE.get()).stream().map(RecipeHolder::value).toList();
+        registration.addRecipes(AnalyzerCategory.ANALYZER_TYPE, AnalyzerRecipes);
+
+        List<VatRecipe> VatRecipes = recipeManager.getAllRecipesFor(ModRecipes.VAT_TYPE.get()).stream().map(RecipeHolder::value).toList();
+        registration.addRecipes(VatCategory.VAT_TYPE, VatRecipes);
+
+        List<CrusherRecipe> CrusherRecipes = recipeManager.getAllRecipesFor(ModRecipes.CRUSHER_TYPE.get()).stream().map(RecipeHolder::value).toList();
+        registration.addRecipes(CrusherCategory.CRUSHER_TYPE, CrusherRecipes);
+
+        List<incubatorRecipe> IncubatorRecipes = recipeManager.getAllRecipesFor(ModRecipes.INCUBATOR_TYPE.get()).stream().map(RecipeHolder::value).toList();
+        registration.addRecipes(IncubatorCategory.INCUBATOR_TYPE, IncubatorRecipes);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(AnalyzerScreen.class, 60, 30, 20, 30,
+                AnalyzerCategory.ANALYZER_TYPE);
+        registration.addRecipeClickArea(VatScreen.class, 60, 30, 20, 30,
+                VatCategory.VAT_TYPE);
+        registration.addRecipeClickArea(CrusherScreen.class, 60, 30, 20, 30,
+                CrusherCategory.CRUSHER_TYPE);
+        registration.addRecipeClickArea(IncubatorScreen.class, 60, 30, 20, 30,
+                IncubatorCategory.INCUBATOR_TYPE);
+    }
+}
---
+++ src/main/java/net/rexbrx/prelude/server/compat/VatCategory.java
@@ -0,0 +1,64 @@
+package net.rexbrx.prelude.server.compat;
+
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.rexbrx.prelude.prelude;
+import net.rexbrx.prelude.server.blocks.PreludeBlocks;
+import net.rexbrx.prelude.server.items.PreludeItems;
+import net.rexbrx.prelude.server.recipes.common.VatRecipe;
+
+public class VatCategory implements IRecipeCategory<VatRecipe> {
+    public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(prelude.MODID, "vat");
+    public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(prelude.MODID,
+            "textures/gui/culture_gui.png");
+
+    public static final RecipeType<VatRecipe> VAT_TYPE =
+            new RecipeType<>(UID, VatRecipe.class);
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public VatCategory(IGuiHelper helper) {
+        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 85);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(PreludeBlocks.VAT.get()));
+    }
+
+    @Override
+    public RecipeType<VatRecipe> getRecipeType() {
+        return VAT_TYPE;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("block.prelude.vat");
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return this.icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, VatRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 80, 6).addIngredients(Ingredient.of(PreludeItems.NUTRIENTS.get()));
+        builder.addSlot(RecipeIngredientRole.INPUT, 57, 18).addIngredients(recipe.getIngredients().get(0));
+        builder.addSlot(RecipeIngredientRole.INPUT, 103, 18).addIngredients(Ingredient.of(PreludeItems.DNA_NUCLEOTIDES.get()));
+
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 80, 60).addItemStack(recipe.getResultItem(null));
+    }
+}
