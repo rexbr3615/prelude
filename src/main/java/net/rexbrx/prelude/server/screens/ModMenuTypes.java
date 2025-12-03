d67a21b52c20e1e52aeffbf7d38743a56b238b43
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2025-12-02 22:35:30 -0300

    pain part 4

---
+++ src/main/java/net/rexbrx/prelude/server/screens/ModMenuTypes.java
@@ -0,0 +1,44 @@
+package net.rexbrx.prelude.server.screens;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
+import net.neoforged.neoforge.network.IContainerFactory;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+import net.rexbrx.prelude.prelude;
+import net.rexbrx.prelude.server.screens.menu.AnalyzerMenu;
+import net.rexbrx.prelude.server.screens.menu.CrusherMenu;
+import net.rexbrx.prelude.server.screens.menu.IncubatorMenu;
+import net.rexbrx.prelude.server.screens.menu.VatMenu;
+
+
+public class ModMenuTypes {
+    public static final DeferredRegister<MenuType<?>> MENUS =
+            DeferredRegister.create(Registries.MENU, prelude.MODID);
+
+
+    public static final DeferredHolder<MenuType<AnalyzerMenu>> ANALYZER_MENU =
+            registerMenuType("analyzer_menu", AnalyzerMenu::new);
+
+    public static final DeferredHolder<MenuType<VatMenu>> VAT_MENU =
+            registerMenuType("vat_menu", VatMenu::new);
+
+    public static final DeferredHolder<MenuType<CrusherMenu>> CRUSHER_MENU =
+            registerMenuType("crusher_menu", CrusherMenu::new);
+
+    public static final DeferredHolder<MenuType<IncubatorMenu>> INCUBATOR_MENU =
+            registerMenuType("incubator_menu", IncubatorMenu::new);
+
+    private static <T extends AbstractContainerMenu>DeferredHolder<MenuType<?>, MenuType<T>> registerMenuType(String name,
+                                                                                                              IContainerFactory<T> factory) {
+        return MENUS.register(name, () -> IMenuTypeExtension.create(factory));
+    }
+
+    public static void register(IEventBus eventBus) {
+        MENUS.register(eventBus);
+    }
+}
