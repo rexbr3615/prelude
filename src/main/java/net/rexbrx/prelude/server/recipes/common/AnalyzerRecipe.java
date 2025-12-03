408e685573e41bf67ddfae93bfc9d6771acc7b8d
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2025-12-02 22:23:12 -0300

    pain part 4

---
+++ src/main/java/net/rexbrx/prelude/server/recipes/common/AnalyzerRecipe.java
@@ -0,0 +1,90 @@
+package net.rexbrx.prelude.server.recipes.common;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
+import net.rexbrx.prelude.prelude;
+import net.rexbrx.prelude.server.recipes.ModRecipes;
+import org.jetbrains.annotations.Nullable;
+
+public record AnalyzerRecipe(Ingredient inputItem, ItemStack output) implements Recipe<AnalyzerRecipeInput> {
+    // inputItem & output ==> Read From JSON File!
+    // GrowthChamberRecipeInput --> INVENTORY of the Block Entity
+
+    @Override
+    public NonNullList<Ingredient> getIngredients() {
+        NonNullList<Ingredient> list = NonNullList.create();
+        list.add(inputItem);
+        return list;
+    }
+
+    @Override
+    public boolean matches(AnalyzerRecipeInput AnalyzerRecipeInput, Level level) {
+        if (level.isClientSide()) {
+            return false;
+        }
+
+        return inputItem.test(AnalyzerRecipeInput.getItem(0));
+    }
+
+    @Override
+    public ItemStack assemble(AnalyzerRecipeInput AnalyzerRecipeInput, HolderLookup.Provider provider) {
+        return output.copy();
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int i, int i1) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResultItem(HolderLookup.Provider provider) {
+        return output;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return ModRecipes.VAT_SERIALIZER.get();
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return ModRecipes.VAT_TYPE.get();
+    }
+
+    public static class Serializer implements RecipeSerializer<AnalyzerRecipe> {
+        public static final MapCodec<AnalyzerRecipe> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
+                Ingredient.CODEC_NONEMPTY.fieldOf("ingredient").forGetter(AnalyzerRecipe::inputItem),
+                ItemStack.CODEC.fieldOf("result").forGetter(AnalyzerRecipe::output)
+        ).apply(inst, AnalyzerRecipe::new));
+
+        public static final StreamCodec<RegistryFriendlyByteBuf, AnalyzerRecipe> STREAM_CODEC =
+                StreamCodec.composite(
+                        Ingredient.CONTENTS_STREAM_CODEC, AnalyzerRecipe::inputItem,
+                        ItemStack.STREAM_CODEC, AnalyzerRecipe::output,
+                        AnalyzerRecipe::new);
+
+        @Override
+        public MapCodec<AnalyzerRecipe> codec() {
+            return CODEC;
+        }
+
+        @Override
+        public StreamCodec<RegistryFriendlyByteBuf, AnalyzerRecipe> streamCodec() {
+            return STREAM_CODEC;
+        }
+    }
+}
