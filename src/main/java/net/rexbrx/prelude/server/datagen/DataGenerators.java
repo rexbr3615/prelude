9be3316f5e929a3534ce728bfb68ee114bc65da2
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2025-12-06 00:09:14 -0300

    pain part 4

--- src/main/java/net/rexbrx/prelude/server/datagen/DataGenerators.java
+++ src/main/java/net/rexbrx/prelude/server/datagen/DataGenerators.java
@@ -17,5 +17,22 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = prelude.MODID)
-public class DataGenerators {
+public class DataGenerators
+{
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
+                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
+        //generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
+        //generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
+        //generator.addProvider(event.includeServer(), new ModDatapackProvider(packOutput, lookupProvider));
+        //generator.addProvider(event.includeServer(), new ModGlobalLootModifierProvider(packOutput, lookupProvider));
+    }
+
 }
