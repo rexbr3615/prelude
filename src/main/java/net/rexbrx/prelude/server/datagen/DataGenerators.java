78b3a8927c75196f3e851d3e4e267485dcc191cd
rexbr_3615 <83562258+rexbr3615@users.noreply.github.com>
2025-12-05 21:51:31 -0300

    pain part 4

---
+++ src/main/java/net/rexbrx/prelude/server/datagen/DataGenerators.java
@@ -0,0 +1,21 @@
+package net.rexbrx.prelude.server.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.rexbrx.prelude.prelude;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@EventBusSubscriber(modid = prelude.MODID)
+public class DataGenerators {
+}
